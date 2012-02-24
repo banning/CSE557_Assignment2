58efff2eb6aeabbd2d30e68bd516efdeadc0318b
Mike Banning <banning@psu.edu>
2012-02-23 23:22:27 -0500

    Implement pseudo code
    Not tested or complete

--- problem1_a.cpp
+++ problem1_a.cpp
@@ -11,13 +11,69 @@
 #include <time.h>
 using namespace std;
 
+void calculateLatency();
+
 int main (int argc, char *argv[])
 {
 	MPI_Init(&argc, &argv);
 
 	// Calculate cache size and average access latencies
+	calculateLatency();
 
 	MPI_Finalize();
+
+	return 0;
+}
+
+// Calculate cache size and average access latencies
+void calculateLatency()
+{
+	// Intialize variables for testing
+	int iInitialSize = 1024;
+	int iNumIterations = 10000;
+	int iIncrement = 1024;
+	int iMaxSize = 100000000;
+	char* x[iInitialSize];
+	char* y[iInitialSize];
+	int iSize = iInitialSize;
+	int iDotProd = 0;
+
+	//fill x and y with 1s
+	for (int j = 0; j < iMaxSize; j++)
+		x[j] = 1;
+
+	for (int k = 0; k < iMaxSize; k++)
+		y[k] = 1;
+
+	// Intialize array to save output
+	int iOutputSize = 1000;
+	int* iTimerStats[iOutputSize];
+	iOutputSize = 0;
+
+	// Loop until array is 100,000,000
+	while (iSize < iMaxSize)
+	{
+		//timer start
+		for (int i = 0; i < iNumIterations; i++)
+		{
+			for (int z = 0; z < iSize; z++)
+			{
+				iDotProd = iDotProd + x[z] * y[z];
+			}
+		}
+		//timer stop	
+
+		//iTimerStats[iOutputSize] = timerOutput;
+		iOutputSize++;
+
+		iSize = iSize + iIncrement;
+		if (iSize > 1024 && iIncrement == 1024)
+			iIncrement = 100 * iIncrement;
+	}
+
+	// Output statistics
+	for (int m; m < iOutputSize; m++)
+		cout <<iTimerStats[m] <<endl;
 
 	return 0;
 }
--- problem1_b.cpp
+++ problem1_b.cpp
@@ -11,13 +11,71 @@
 #include <time.h>
 using namespace std;
 
+void calculateLatency();
+
 int main (int argc, char *argv[])
 {
 	MPI_Init(&argc, &argv);
 
 	// Calculate random access latency
+	calculateLatency();
 
 	MPI_Finalize();
+
+	return 0;
+}
+
+// Calculate random access latency
+void calculateLatency()
+{
+	// Intialize variables for testing
+	int iInitialSize = 100000000;
+	int iNumIterations = 10000;
+	int iIncrement = 1024;
+	int iMaxSize = 100000000;
+	char* x[iInitialSize];
+	char* y[iInitialSize];
+	int iSize = iInitialSize;
+	int iDotProd = 0;
+
+	//fill x and y with 1s
+	for (int j = 0; j < iMaxSize; j++)
+		x[j] = 1;
+
+	for (int k = 0; k < iMaxSize; k++)
+		y[k] = 1;
+
+	// Intialize array to save output
+	int iOutputSize = 1000;
+	int* iTimerStats[iOutputSize];
+	iOutputSize = 0;
+
+	// Loop until array is 100,000,000
+	while (iSize < iMaxSize)
+	{
+		//timer start
+		for (int i = 0; i < iNumIterations; i++)
+		{
+			for (int z = 0; z < iInitialSize; z++)
+			{
+				//Ww1 = random_number(0 to 1) * iSize
+				//Ww2 = random_number(0 to 1) * iSize
+				//iDotProd = iDotProd + x[Ww1] * y[Ww2];
+			}
+		}
+		//timer stop	
+
+		//iTimerStats[iOutputSize] = timerOutput;
+		iOutputSize++;
+
+		// iSize = iSize + iIncrement;
+		// if (iSize > 1024 && iIncrement == 1024)
+		// 	iIncrement = 100 * iIncrement;
+	}
+
+	// Output statistics
+	for (int m; m < iOutputSize; m++)
+		cout <<iTimerStats[m] <<endl;
 
 	return 0;
 }
