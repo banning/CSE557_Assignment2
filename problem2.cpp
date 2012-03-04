c0e9a277dd56c25ffa54ed44d94ae0408a41d5cc
dsk5026 <dsk5026@psu.edu>
2012-03-04 11:54:59 -0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,23 +1,72 @@
-// CSE557 Assignment 2
-// Problem 2
-// Calculate commuication latency
-// 03/12/12
-// Mike Banning
-// Dan Keating
-
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 using namespace std;
+
+double  tsFloat (timespec  time)
+{
+    return ((double) time.tv_sec + (time.tv_nsec / 1000000000.0)) ;
+
+} 
+
+// Implement HyperQuicksort
+void Bandwidth()
+{
+   int rank, size, source, destination, token;
+   MPI_Comm_size(MPI_COMM_WORLD, &size);  
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   double start, end;
+   int iterations=1000;
+   double difList[size];
+   MPI_Status status;
+
+   source=0;
+   for (int destination = source+1; destination < size; destination++)
+   {
+      //clock_gettime(CLOCK_REALTIME, &start);
+      start=MPI_Wtime();
+      if (rank == source || rank == destination)
+      {
+         for (int i = 0; i < iterations; i++)
+         {
+            // Check if lead processor
+            if (rank == source)
+            {
+               //Broadcast listsize to all processors
+               MPI_Send(&token, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
+               MPI_Recv(&token, 1, MPI_INT, destination, 0, MPI_COMM_WORLD, &status);
+            }
+            else if (rank==destination)
+            {
+               //Recieve listsize from lead processor
+               MPI_Recv(&token, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+               MPI_Send(&token, 1, MPI_INT, source, 0, MPI_COMM_WORLD);
+            }
+         }
+         if (rank==source)
+         {
+            end=MPI_Wtime();
+            difList[destination]=end-start;
+         }
+      }
+   }
+   if (rank==0)
+   {
+      cout<<"Timing data is as follows\n";
+      for (int i = 1; i < size; i++)
+      {
+         cout<<"destination "<<i<<" time:" <<difList[i]<<'\n';
+      }
+   }      
+}
+
 
 int main (int argc, char *argv[])
 {
-	MPI_Init(&argc, &argv);
 
-	// Calculate commuication latency
-
-	MPI_Finalize();
-	
-	return 0;
+   MPI_Init(&argc, &argv);
+   Bandwidth();
+   MPI_Finalize();
+   return 0;
 }
